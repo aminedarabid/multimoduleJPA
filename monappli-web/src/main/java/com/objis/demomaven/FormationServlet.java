6ca9614321fdb391a7ae69eaf01e06706f5f28fb
Rudy <rudy.tourrou@live.fr>
2019-02-13 11:43:06 +0100

    [Rudy] Attempt #1 at improving vulnerabilities

--- monappli-web/src/main/java/com/objis/demomaven/FormationServlet.java
+++ monappli-web/src/main/java/com/objis/demomaven/FormationServlet.java
@@ -1,5 +1,7 @@
 package com.objis.demomaven;
 
+import com.objis.demomaven.domaine.Formation;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.servlet.ServletException;
@@ -7,57 +9,54 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.objis.demomaven.domaine.Formation;
-
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Servlet implementation class FormationServlet
+ */
+@WebServlet("/FormationServlet")
+public class FormationServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     /**
-     * Servlet implementation class FormationServlet
+     * @see HttpServlet#HttpServlet()
      */
-    @WebServlet("/FormationServlet")
-    public class FormationServlet extends HttpServlet {
-        private static final long serialVersionUID = 1L;
-
-        /**
-         * @see HttpServlet#HttpServlet()
-         */
-        public FormationServlet() {
-            super();
-        }
-
-        /**
-         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-         */
-        @Override
-        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-            // 1. Obtenir une connection database
-            EntityManagerFactory emf = (EntityManagerFactory)getServletContext().getAttribute("emf");
-            IFormationService monservice = new FormationService();
-
-
-            try {
-                // 2. Création d'une nouvelle formation ?:
-                String theme = request.getParameter("themeFormation");
-                if (theme != null) {
-                	Formation maformation = new Formation(theme);
-                	monservice.creerFormation(emf, maformation);
-                }
-
-                // 3. Afficher la liste des formations en Base:
-                List<Formation> listeFormations = monservice.lireLesFormations(emf);
-                
-                request.setAttribute("listeDesFormations", listeFormations);
-                request.getRequestDispatcher("/index.jsp").forward(request, response);
-
-            } finally {
-                // 4. fermer la connection
-            	EntityManager em = emf.createEntityManager();
-                if (em.getTransaction().isActive())
-                    em.getTransaction().rollback();
-                em.close();
-            }
-        }
-
+    public FormationServlet() {
+        super();
     }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 1. Obtenir une connection database
+        EntityManagerFactory emf = (EntityManagerFactory) getServletContext().getAttribute("emf");
+        IFormationService monservice = new FormationService();
+
+        try {
+            // 2. Création d'une nouvelle formation ?:
+            String theme = request.getParameter("themeFormation");
+            if (theme != null) {
+                Formation maformation = new Formation(theme);
+                monservice.creerFormation(emf, maformation);
+            }
+
+            // 3. Afficher la liste des formations en Base:
+            List<Formation> listeFormations = monservice.lireLesFormations(emf);
+
+            request.setAttribute("listeDesFormations", listeFormations);
+
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        } catch (ServletException | IOException e) {
+            System.out.print(e.getMessage());
+        }
+
+        // 4. Fermer la connection
+        EntityManager em = emf.createEntityManager();
+        if (em.getTransaction().isActive())
+            em.getTransaction().rollback();
+        em.close();
+    }
+}
